4065f978f96f08efa0d034a39469c122abe9c30a
Kestener <r.kestener@gmail.com>
2023-03-30 17:51:03 +0200

    landslide inference app on streamlit v.0001

--- interface/pages/2_Landslide_Inference_Tool.py
+++ interface/pages/2_Landslide_Inference_Tool.py
@@ -1,27 +1,29 @@
 import io
 from PIL import Image
 import streamlit as st
-import torch
-from torchvision import transforms
+from ultralytics import YOLO
+import time
 
 
-MODEL_PATH = '../data/models/model.pt'
-LABELS_PATH = '../data/models/model_classes.txt'
+MODEL_PATH = '../../data/models/YOLOv8_Landslide_WorkingV1_best.pt'
+LABELS_PATH = '../../data/models/model_classes.txt'
 
 
 def load_image():
     uploaded_file = st.file_uploader(label='Pick an image to test')
     if uploaded_file is not None:
-        image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        return Image.open(io.BytesIO(image_data))
+        st.write(uploaded_file)
+        img = Image.open(uploaded_file)
+        #image_data = uploaded_file.getvalue()
+        st.image(img)
+        #return Image.open(io.BytesIO(image_data))
+        return img
     else:
         return None
 
 
 def load_model(model_path):
-    model = torch.load(model_path, map_location='cpu')
-    model.eval()
+    model = YOLO(model_path)
     return model
 
 
@@ -31,35 +33,37 @@ def load_labels(labels_file):
         return categories
 
 
-def predict(model, categories, image):
-    preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    input_tensor = preprocess(image)
-    input_batch = input_tensor.unsqueeze(0)
+def mod_predict(model, input_image):
+    result = model.predict(input_image)
+    return result
 
-    with torch.no_grad():
-        output = model(input_batch)
+def mod_predict2(model, input_image):
+    im1 = input_image
+    results = model.predict(source=im1, save=True)  # save plotted images
+    return results
 
-    probabilities = torch.nn.functional.softmax(output[0], dim=0)
-
-    all_prob, all_catid = torch.topk(probabilities, len(categories))
-    for i in range(all_prob.size(0)):
-        st.write(categories[all_catid[i]], all_prob[i].item())
 
 
 def main():
     st.title('Custom model demo')
     model = load_model(MODEL_PATH)
-    categories = load_labels(LABELS_PATH)
+    #categories = load_labels(LABELS_PATH)
     image = load_image()
     result = st.button('Run on image')
     if result:
         st.write('Calculating results...')
-        predict(model, categories, image)
+        #mod_predict2(model, image)
+        im1 = Image.open('../../data/satellite_images_train_v1/satellite_predict_test_y.jpg')
+        results = model.predict(source=im1, save=True)
+        st.image('runs/detect/predict/satellite_predict_test_y.jpg')
+
+#        import glob
+#        import os
+
+#        list_of_files = glob.glob('/path/to/folder/*') # * means all if need specific format then *.csv
+#        latest_file = max(list_of_files, key=os.path.getctime)
+#        print(latest_file)
+        
 
 
 if __name__ == '__main__':
